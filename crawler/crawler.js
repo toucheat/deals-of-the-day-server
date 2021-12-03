8b5e1f1a13fc367ff3467d37da3f71443c3c2fa0
CJ Kim <zxagsw@gmail.com>
2021-12-04 01:39:56 +0900

    work crawler

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -8,8 +8,9 @@ const log = console.log;
 
 router.get('/:id', function(req, res) {
     const getHtml = async () => {
-        try {
-          return await axios.get("http://search.danawa.com/dsearch.php?k1=" + req.params.id);
+      log(req.params.id);
+      try {
+          return await axios.get("http://search.danawa.com/dsearch.php?k1=" + encodeURIComponent(req.params.id));
         } catch (error) {
           console.error(error);
         }
@@ -17,21 +18,27 @@ router.get('/:id', function(req, res) {
 
       getHtml()
       .then(html => {
-        let ulList = [];
+        let result = [];
         const $ = cheerio.load(html.data);
-        const $bodyList = $("div.main_prodlist.main_prodlist_list.product_list").children(li.prod_item);
-    
-        $bodyList.each(function(i, elem) {
-          ulList[i] = {
-            url: $(this).find('p.prod_name a').attr('href'),
-          };
-        });
-    
-        const data = ulList.filter(n => n.title);
-        return data;
-      })
-      .then(res => log(res));
-});
+        
+        const list = $("#productListArea > div.main_prodlist.main_prodlist_list > ul.product_list > li").toArray();
 
+        list.forEach((src) => {
+          const select = $(src).find("div.prod_main_info > div.prod_info > p.prod_name > a");
+          const name = select.text();
+          const link = select.attr("href");
+          const price = $(src).find("div.prod_main_info > div.prod_pricelist > ul > li > p.price_sect > a").first().text();
+
+          result.push({
+            name,
+            link,
+            price,
+          });
+        });
+
+        return result;
+      })
+      .then(ress => log(ress));
+});
 
 module.exports = router;
