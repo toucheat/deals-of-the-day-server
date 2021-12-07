f480a4f7343f373d82673f46426315541073e147
CJ Kim <zxagsw@gmail.com>
2021-12-07 17:15:02 +0900

    fix user search, delete

--- user/UserController.js
+++ user/UserController.js
@@ -23,17 +23,17 @@ router.get('/', function(req, res) {
         res.status(200).send(users);
     });
 });
-// User 조회
-router.get('/:id', function(req, res) {
-    User.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("User 조회 실패");
-        if (!user) return res.status(404).send("User 없음.");
+// User 이메일로 찾기
+router.get('/:email', function(req, res) {
+    User.findOne( { email: req.params.email }, function (err, user) {
+        if (err) return res.status(500).send("이메일 조회 실패");
+        if (!user) return res.status(404).send("매치되는 이메일 없음.");
         res.status(200).send(user);
     });
 });
 // User 삭제
 router.delete('/:id', function (req, res) {
-    User.findByIdAndRemove(req.params.id, function (err, user) {
+    User.findOneAndDelete( { email: req.params.id }, function (err, user) {
         if (err) return res.status(500).send("User 삭제 실패");
         res.status(200).send("User "+ user.name +" 삭제됨.");
     });
@@ -44,22 +44,5 @@ router.put('/:id', function (req, res) {
         if (err) return res.status(500).send("User 수정 실패.");
         res.status(200).send(user);
     });
-});
-//이메일로 찾기
-router.get('/findemail/:email', function(req, res) {
-    User.findOne( { email: req.params.email }, function (err, user) {
-        if (err) return res.status(500).send("이메일 조회 실패");
-        if (!user) return res.status(404).send("매치되는 이메일 없음.");
-        res.status(200).send(user);
-    });
-});
-//찜하기 추가
-router.post('/addfavorite', function(req, res) {
-    var favorite = {"pcode": req.body.pcode};
-    User.findOneAndUpdate({ email: req.body.email }, { $push: { favoriteList: favorite }},
-        function(err, user) {
-            if (err) return res.status(500).send("업데이트 실패.");
-            res.status(200).send("add success");
-        });
 });
 module.exports = router;
