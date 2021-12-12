ad34e9f92007b9673881a3f1a4d07ca535a94da0
CJ Kim <zxagsw@gmail.com>
2021-12-12 21:55:28 +0900

    modified load speed

--- explore/explore.js
+++ explore/explore.js
@@ -49,7 +49,14 @@ router.get('/', function(req, res) {
             '--single-process'
           ];
 
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ args:['--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu'], headless: true });
         const page = await browser.newPage();
 
         await page.goto('http://danawa.com');
