ec571aaf08c3b5de3f4ec14aac115810d1eb66e4
CJ Kim <zxagsw@gmail.com>
2021-12-12 22:00:03 +0900

    modified load speed

--- explore/explore.js
+++ explore/explore.js
@@ -8,48 +8,15 @@ const puppeteer = require('puppeteer');
 router.get('/', function(req, res) {
     (async() => {
         result = [];
-        const minimal_args = [
-            '--autoplay-policy=user-gesture-required',
-            '--disable-background-networking',
-            '--disable-background-timer-throttling',
-            '--disable-backgrounding-occluded-windows',
-            '--disable-breakpad',
-            '--disable-client-side-phishing-detection',
-            '--disable-component-update',
-            '--disable-default-apps',
-            '--disable-dev-shm-usage',
-            '--disable-domain-reliability',
-            '--disable-extensions',
-            '--disable-features=AudioServiceOutOfProcess',
-            '--disable-hang-monitor',
-            '--disable-ipc-flooding-protection',
-            '--disable-notifications',
-            '--disable-offer-store-unmasked-wallet-cards',
-            '--disable-popup-blocking',
-            '--disable-print-preview',
-            '--disable-prompt-on-repost',
-            '--disable-renderer-backgrounding',
-            '--disable-setuid-sandbox',
-            '--disable-speech-api',
-            '--disable-sync',
-            '--hide-scrollbars',
-            '--ignore-gpu-blacklist',
-            '--metrics-recording-only',
-            '--mute-audio',
-            '--no-default-browser-check',
-            '--no-first-run',
-            '--no-pings',
-            '--no-sandbox',
-            '--no-zygote',
-            '--password-store=basic',
-            '--use-gl=swiftshader',
-            '--use-mock-keychain',
-            '--disable-accelerated-2d-canvas',
-            '--disable-gpu',
-            '--single-process'
-          ];
 
-        const browser = await puppeteer.launch({ args: minimal_args, headless: true });
+        const browser = await puppeteer.launch({ args:['--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu'], headless: true });
         const page = await browser.newPage();
 
         await page.goto('http://danawa.com');
